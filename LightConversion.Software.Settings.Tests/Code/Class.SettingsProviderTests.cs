4c5b11232f33b4f848560f506c026f3a08ed9c29
Matas Girdauskas <matas.girdauskas@lightcon.com>
2020-01-20 18:54:35 +0200

    Wiring few failing tests (Case 4462).

--- LightConversion.Software.Settings.Tests/Code/Class.SettingsProviderTests.cs
+++ LightConversion.Software.Settings.Tests/Code/Class.SettingsProviderTests.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LightConversion.Software.Settings.Tests {
@@ -84,6 +85,59 @@ namespace LightConversion.Software.Settings.Tests {
   ""SomeStringKey"": ""Hello world""
 }";
             Assert.AreEqual(expectedFileContents, actualFileContents);
+        }
+
+        [TestMethod]
+        public void TestNumberConversion() {
+            CreateCleanTempFolder();
+
+            File.WriteAllText("temp/someSettings.json", @"{
+                ""SomeIntegerKey"": 123.456,
+                ""SomeIntegerKey2"": 123.5
+            }");
+
+            var settings = new SettingsProvider();
+            settings.Initialize("temp/someSettings.json");
+
+            var isOk = settings.TryGet("SomeIntegerKey", out int someInteger);
+            Assert.IsTrue(isOk);
+            Assert.AreEqual(123, someInteger);
+
+            isOk = settings.TryGet("SomeIntegerKey2", out int someInteger2);
+            Assert.IsTrue(isOk);
+            Assert.AreEqual(124, someInteger2, "Value should probably be rounded");
+        }
+
+        [TestMethod]
+        public void TestDefaultValues() {
+            CreateCleanTempFolder();
+
+            var settings = new SettingsProvider();
+            settings.Initialize("temp/someSettings.json");
+
+            var isOk = settings.TryGet("NonExistingKey", out int value, true, 123);
+            Assert.IsTrue(isOk);
+            Assert.AreEqual(123, value);
+
+            // Value should now be set to default after last operation.
+            isOk = settings.TryGet("NonExistingKey", out value);
+            Assert.IsTrue(isOk);
+            Assert.AreEqual(123, value);
+        }
+
+        [TestMethod]
+        public void TestInitializationWithInvalidJson() {
+            CreateCleanTempFolder();
+
+            File.WriteAllText("temp/someSettings.json", "Invalid json text");
+
+            var settings = new SettingsProvider();
+
+            try {
+                settings.Initialize("temp/someSettings.json");
+            } catch (Exception ex) {
+                Assert.Fail("Initialization shouldn't throw any exceptions", ex);
+            }
         }
 
         private void CreateCleanTempFolder() {
