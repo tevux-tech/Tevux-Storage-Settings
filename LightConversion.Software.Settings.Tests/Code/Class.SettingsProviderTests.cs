720b4655c7cd2dd038ac073b18e93bd38bfab043
Matas Girdauskas <matas.girdauskas@lightcon.com>
2020-01-20 18:12:07 +0200

    Prototyping (Case 4462).

---
+++ LightConversion.Software.Settings.Tests/Code/Class.SettingsProviderTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Configuration;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LightConversion.Software.Settings.Tests {
+    [TestClass]
+    public class SettingsProviderTests {
+        public double Test;
+
+
+        [TestMethod]
+        public void TestBasicSetGet() {
+            CreateCleanTempFolder();
+
+            var settings = new SettingsProvider();
+            settings.Initialize("temp/someSettings.json");
+
+            settings.TrySet("SomeIntegerKey", 123);
+            settings.TryGet("SomeIntegerKey", out int someIntegerValue);
+            Assert.AreEqual(123, someIntegerValue);
+
+            settings.TrySet("SomeDoubleKey", 123.456);
+            settings.TryGet("SomeDoubleKey", out double someDoubleValue);
+            Assert.AreEqual(123.456, someDoubleValue);
+
+            settings.TrySet("SomeBoolKey", true);
+            settings.TryGet("SomeBoolKey", out bool someBoolValue);
+            Assert.AreEqual(true, someBoolValue);
+
+            settings.TrySet("SomeStringKey", "Some string");
+            settings.TryGet("SomeStringKey", out string someStringValue);
+            Assert.AreEqual("Some string", someStringValue);
+        }
+
+        private void CreateCleanTempFolder() {
+            if (Directory.Exists("temp")) {
+                Directory.Delete("temp", true);
+            }
+
+            Directory.CreateDirectory("temp");
+        }
+    }
+}
---
+++ LightConversion.Software.Settings/Code/Class.SettingsProvider.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace LightConversion.Software.Settings {
+    public class SettingsProvider : IDisposable {
+        private Dictionary<string, object> _dataCache;
+        private readonly object _dataCacheLock = new object();
+        private ReliableFile _dataFile;
+        public bool IsInitialized { get; private set; }
+
+        public void Initialize(string storageFilePath) {
+            _dataFile = new ReliableFile(storageFilePath);
+
+            if (_dataFile.TryReadAllText(out var fileContents)) {
+                // TODO: what if invalid json is saved?
+                _dataCache = Utf8Json.JsonSerializer.Deserialize<Dictionary<string, object>>(fileContents);
+            } else {
+                // Probably file doesn't exist, initializing to empty stuff. 
+                _dataFile.TryWriteAllText("{}");
+                _dataCache = new Dictionary<string, object>();
+            }
+
+            IsInitialized = true;
+        }
+
+        public void Dispose() {}
+
+        public bool Exists(string key) {
+            return true;
+        }
+
+        public bool TrySet(string key, int value) {
+            lock (_dataCacheLock) {
+                _dataCache[key] = value;
+            }
+
+            return true;
+        }
+
+        public bool TrySet(string key, double value) {
+            lock (_dataCacheLock) {
+                _dataCache[key] = value;
+            }
+
+            return true;
+        }
+
+        public bool TrySet(string key, string value) {
+            lock (_dataCacheLock) {
+                _dataCache[key] = value;
+            }
+
+            return true;
+        }
+
+        public bool TrySet(string key, bool value) {
+            lock (_dataCacheLock) {
+                _dataCache[key] = value;
+            }
+
+            return true;
+        }
+
+        public bool TryGet(string key, out int value, bool setToDefaultIfDoesNotExist = false, int defaultValue = 0) {
+            bool isOk;
+            value = 0;
+
+            object valueOfUnknownType;
+
+            lock (_dataCacheLock) {
+                isOk = _dataCache.TryGetValue(key, out valueOfUnknownType);
+            }
+
+            if (isOk) {
+                isOk = TryConvertToInt(valueOfUnknownType, out value);
+            }
+
+            return isOk;
+        }
+
+        public bool TryGet(string key, out double value, bool setToDefaultIfDoesNotExist = false, double defaultValue = 0) {
+            bool isOk;
+            value = 0;
+
+            object valueOfUnknownType;
+
+            lock (_dataCacheLock) {
+                isOk = _dataCache.TryGetValue(key, out valueOfUnknownType);
+            }
+
+            if (isOk) {
+                isOk = TryConvertToDouble(valueOfUnknownType, out value);
+            }
+
+            return isOk;
+        }
+
+        public bool TryGet(string key, out string value, bool setToDefaultIfDoesNotExist = false, string defaultValue = null) {
+            bool isOk;
+            value = "";
+
+            object valueOfUnknownType;
+
+            lock (_dataCacheLock) {
+                isOk = _dataCache.TryGetValue(key, out valueOfUnknownType);
+            }
+
+            if (isOk) {
+                isOk = TryConvertToString(valueOfUnknownType, out value);
+            }
+
+            return isOk;
+        }
+
+        public bool TryGet(string key, out bool value, bool setToDefaultIfDoesNotExist = false, bool defaultValue = false) {
+            bool isOk;
+            value = false;
+
+            object valueOfUnknownType;
+
+            lock (_dataCacheLock) {
+                isOk = _dataCache.TryGetValue(key, out valueOfUnknownType);
+            }
+
+            if (isOk) {
+                isOk = TryConvertToBool(valueOfUnknownType, out value);
+            }
+
+            return isOk;
+        }
+
+        private bool TryConvertToInt(object valueToConvert, out int convertedValue) {
+            var isOk = true;
+
+            if (valueToConvert is int valueAsInt) {
+                convertedValue = valueAsInt;
+            } else if (valueToConvert is long valueAsLong) {
+                convertedValue = (int)valueAsLong;
+            } else if (valueToConvert is double valueAsDouble) {
+                convertedValue = Convert.ToInt32(valueAsDouble);
+            } else {
+                convertedValue = 0;
+                isOk = false;
+            }
+
+            return isOk;
+        }
+
+        private bool TryConvertToDouble(object valueToConvert, out double convertedValue) {
+            var isOk = true;
+
+            if (valueToConvert is int valueAsInt) {
+                convertedValue = valueAsInt;
+            } else if (valueToConvert is long valueAsLong) {
+                convertedValue = valueAsLong;
+            } else if (valueToConvert is double valueAsDouble) {
+                convertedValue = valueAsDouble;
+            } else {
+                convertedValue = 0;
+                isOk = false;
+            }
+
+            return isOk;
+        }
+
+        private bool TryConvertToBool(object valueToConvert, out bool convertedValue) {
+            bool isOk;
+
+            if (valueToConvert is bool valueAsBool) {
+                isOk = true;
+                convertedValue = valueAsBool;
+            } else {
+                isOk = false;
+                convertedValue = false;
+            }
+
+            return isOk;
+        }
+
+        private bool TryConvertToString(object valueToConvert, out string convertedValue) {
+            if (valueToConvert is string valueAsString) {
+                convertedValue = valueAsString;
+                return true;
+            } else {
+                convertedValue = "";
+                return false;
+            }
+        }
+    }
+}
