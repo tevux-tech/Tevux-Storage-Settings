806471ab36a74aee2c5ca25adcbc544e4ee82039
Matas Girdauskas <matas.girdauskas@lightcon.com>
2020-01-23 15:38:58 +0200

    Trying to implement ISilentReporter for SettingsProvider (Case 4346).

--- LightConversion.Software.Settings/Code/Class.SettingsProvider/Class.SettingsProvider.cs
+++ LightConversion.Software.Settings/Code/Class.SettingsProvider/Class.SettingsProvider.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections.Generic;
 
 namespace LightConversion.Software.Settings {
-    public partial class SettingsProvider {
+    public partial class SettingsProvider : ISilentReporter {
         private Dictionary<string, object> _dataCache;
         private readonly object _dataLock = new object();
         public ReliableFile DataFile { get; private set; }
 
         public bool IsInitialized { get; private set; }
+
+        public bool AreExceptionsSilent { get; set; }
+
+        public event GeneralEventHandler ErrorOccurred;
+        public event GeneralEventHandler InfoReady;
     }
 }
